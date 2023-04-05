551ca6342a7f7821e0a1f544f0d98ac4e481560a
Александр Седенко <sedslexst@gmail.com>
2023-04-05 15:08:14 +0300

    splitting text into words

--- M01HW4/Program.cs
+++ M01HW4/Program.cs
@@ -3,6 +3,28 @@ using System.Text.RegularExpressions;
 
 class Program
 {
+    static void Main(string[] args)
+    {
+        // ввод текста с датой
+        Console.WriteLine("Введите текст с датой в формате dd.MM.yyyy:");
+        string input = Console.ReadLine();
+
+
+        // удаление цифр из текста
+        string noDigits = Regex.Replace(input, @"\d+", "");
+
+        // разделение текста на слова и замена букв в нечетных словах
+        string[] words = noDigits.Split(' ');
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (i % 2 == 0) continue;
+            char[] chars = words[i].ToCharArray();
+            Array.Reverse(chars);
+            words[i] = new string(chars);
+        }
+        string withReversedChars = string.Join(" ", words);
+
+    }
 
 
 
